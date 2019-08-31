f40690851a4be4683d1092445e47221d85057997
Andrew Maraev <the_vk@thevk.net>
2019-08-31 12:16:23 -0700

    architecture for model change event notification stream
    * implemented interface ObservableModel in TodoApp
    * deleted obsolete interface EventChannels

--- src/model/event_chanels.ts
+++
@@ -1,7 +0,0 @@
-import { Observable } from 'rxjs';
-
-import { TodoList } from './todo_list';
-
-export interface EventChannels {
-    getModelStateChannel(): Observable<TodoList>;
-}
--- src/model/todo_app.ts
+++ src/model/todo_app.ts
@@ -1,19 +1,22 @@
-import { EventChannels } from './event_chanels';
-import { TodoList } from './todo_list';
 import { Observable } from 'rxjs';
+import { mapTo } from 'rxjs/operators';
 
-export class TodoApp implements EventChannels {
+import { ObservableModel } from './observable_model';
+import { TodoList } from './todo_list';
+
+export class TodoApp implements ObservableModel<TodoApp> {
     private _todoList: TodoList;
-
     public getTodosList(): TodoList {
         return this._todoList;
+    }
+
+    getChangedObservable(): Observable<TodoApp> {
+        return this._todoList.getChangedObservable().pipe(
+            mapTo(this)
+        );
     }
     
     constructor() {
         this._todoList = new TodoList();
-    }
-    
-    getModelStateChannel(): Observable<TodoList> {
-        return this._todoList.getStateEventChannel();
     }
 }
